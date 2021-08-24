f28387178130446c90b0701aa468172d90b2a946
Vinicius De Paula <viniciusdepaula199@gmail.com>
2021-08-24 02:56:41 -0300

    xd

--- src/sync/SyncBackup.java
+++ src/sync/SyncBackup.java
@@ -14,11 +14,49 @@ public class SyncBackup implements PubSubCommand {
     @Override
     public Message execute(Message m, SortedSet<Message> log, Set<String> subscribers, boolean isPrimary,
                            String sencondaryServerAddress, int secondaryServerPort, boolean secActivity) {
-        
+        if (!log.isEmpty()) {
+            Iterator<Message> it = log.iterator();
+            while (it.hasNext()) {                
+                try {
+                    Message msg = it.next();
+                    Message syncPubMsg = new MessageImpl();
+                    syncPubMsg.setBrokerId(msg.getBrokerId());
+                    syncPubMsg.setContent(msg.getContent());
+                    syncPubMsg.setLogId(msg.getLogId());
+                    syncPubMsg.setType("syncPub");
+
+                    Client clientBackup = new Client(sencondaryServerAddress, secondaryServerPort);
+                    syncPubMsg = clientBackup.sendReceive(syncPubMsg);
+
+                } catch (Exception e) {}
+            }
+        }
+
+        System.out.println("SUBSCRIBERSS");
+        if (!subscribers.isEmpty()) {
+            Iterator<String> it = subscribers.iterator();
+            while (it.hasNext()) {                
+                try {
+                    String msg = it.next();
+                    Message syncPubMsg = new MessageImpl();
+                    syncPubMsg.setBrokerId(secondaryServerPort);
+                    syncPubMsg.setContent(msg);
+                    syncPubMsg.setType("syncSub");
+                    System.out.println(msg);
+
+                    Client clientBackup = new Client(sencondaryServerAddress, secondaryServerPort);
+                    syncPubMsg = clientBackup.sendReceive(syncPubMsg);
+
+                } catch (Exception e) {}
+            }
+        }
+
 
         secActivity = true;
 
         Message response = new MessageImpl();
+        response.setLogId(m.getLogId());
+        response.setContent("Message published on backup: " + m.getContent());
         response.setType("backup_ack");
 
         return response;
